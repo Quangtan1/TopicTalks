566bbaf6fe8d0f21466d66de8f59756ce6c92742
Son Le <son_le@datahouse.com>
2023-10-21 21:29:46 +0700

    feat: answer modal

--- client/src/components/admin/manageusersQA/ManageUsersQA.tsx
+++ client/src/components/admin/manageusersQA/ManageUsersQA.tsx
@@ -13,16 +13,23 @@ import {
   Typography,
 } from '@mui/material';
 import dayjs from 'dayjs';
-import React, { useEffect, memo, useState } from 'react';
-import { GrView } from 'react-icons/gr';
+import { isEmptyArray } from 'formik';
+import { useEffect, memo, useState } from 'react';
 import { MdOutlineErrorOutline } from 'react-icons/md';
 import accountStore from 'src/store/accountStore';
 import uiStore from 'src/store/uiStore';
 import { createAxios, getDataAPI } from 'src/utils';
-import { ToastSuccess } from 'src/utils/toastOptions';
-, postDataAPI
-const ManageUserQA = () => {
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { observer } from 'mobx-react';
+import AnswerQAModal from './answerModal';
+
+const USER_IMAGE = 'https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg';
+
+const ManageUserQA = observer(() => {
+  dayjs.extend(relativeTime);
   const [usersQA, setUsersQA] = useState([]);
+  const [isOpenAnswerModal, setIsOpenAnswerModal] = useState(false);
+  const [userQA, setUserQA] = useState({});
   const account = accountStore?.account;
   const setAccount = () => {
     return accountStore?.setAccount;
@@ -48,100 +55,92 @@ const ManageUserQA = () => {
       });
   }, []);
 
-    const handleAnswer = (id) => {
-      const qaAnswerData = {
-         qaId:id,
-         adminReplyId:account?.id,
-       content:''
-      };
-      postDataAPI('/qa/create', qaAnswerData, account.access_token, axiosJWT)
-        .then((res) => {
-          ToastSuccess('Send Question Successfully!!!');
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-  };
-
   const handleChangePage = (event, newPage) => {};
 
   const handleChangeRowsPerPage = (event) => {};
 
+  const handleOpenAnswerModal = (item) => {
+    setIsOpenAnswerModal((prev) => !prev);
+    setUserQA(item);
+  };
+
+  const userList = usersQA || [];
+
   return (
-    <Box className="manage_post_container">
-      <Box className="post_title">
-        <Typography className="title_a1">List of posts waiting to be approve</Typography>
+    <>
+      <Box className="manage_post_container">
+        <Box className="post_title">
+          <Typography className="title_a1">List of QA from Admin</Typography>
+        </Box>
+        <Typography className="title_a2">Manage user's QA in system</Typography>
+        <Box className="warning">
+          <MdOutlineErrorOutline />
+          <Typography>Please answer user questions about the system!</Typography>
+        </Box>
+        <TableContainer className="table_container_head">
+          <Table>
+            <TableHead className="table_head">
+              <TableRow>
+                <TableCell className="cell_no">Question Id</TableCell>
+                <TableCell className="cell_content">Subject</TableCell>
+                <TableCell className="cell_content">Content</TableCell>
+                <TableCell className="cell_createdAt">Created Time</TableCell>
+                <TableCell className="cell_img">Image</TableCell>
+                <TableCell className="cell_action">Action</TableCell>
+              </TableRow>
+            </TableHead>
+          </Table>
+        </TableContainer>
+        <TableContainer className="table_container_body">
+          <Table>
+            <TableBody className="table_body">
+              {!isEmptyArray(userList) &&
+                userList.map((item, index: number) => {
+                  const timeAgo = dayjs(item?.created_at).fromNow();
+                  return (
+                    <TableRow key={item?.id}>
+                      <TableCell className="cell_id">{item?.id}</TableCell>
+                      <TableCell className="cell_content">
+                        <Tooltip title={item?.title} arrow>
+                          <span className="cell_content">
+                            {item?.subject?.length > 15 ? `${item?.subject?.slice(0, 15)}...` : item?.subject}
+                          </span>
+                        </Tooltip>
+                      </TableCell>
+                      <TableCell className="cell_content">
+                        <Tooltip title={item?.content} arrow>
+                          <span className="cell_content">
+                            {item?.content?.length > 30 ? `${item?.content?.slice(0, 30)}...` : item?.content}
+                          </span>
+                        </Tooltip>
+                      </TableCell>
+                      <TableCell className="cell_createdAt">{timeAgo}</TableCell>
+                      <TableCell className="cell_img">
+                        {<img src={item?.senderInfor?.avatarUrl || USER_IMAGE} alt="img" className="img" />}
+                      </TableCell>
+                      <TableCell className="cell_action">
+                        <Button onClick={() => handleOpenAnswerModal(item)}>Answer ❓❓❓</Button>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          className="table_pagination"
+          component="div"
+          count={count}
+          page={page}
+          rowsPerPage={rowsPerPage}
+          rowsPerPageOptions={rowsPerPageOptions}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+        <AnswerQAModal isOpen={isOpenAnswerModal} questionData={userQA} setIsOpenAnswerModal={setIsOpenAnswerModal} />
       </Box>
-      <Typography className="title_a2">Manage user's post in system</Typography>
-      <Box className="warning">
-        <MdOutlineErrorOutline />
-        <Typography>Please monitor whether user's post are violating community content</Typography>
-      </Box>
-      <TableContainer className="table_container_head">
-        <Table>
-          <TableHead className="table_head">
-            <TableRow>
-              <TableCell className="cell_no">No.</TableCell>
-              <TableCell className="cell_no">Post Id</TableCell>
-              <TableCell className="cell_content">Subject</TableCell>
-              <TableCell className="cell_content">Content</TableCell>
-              <TableCell className="cell_createdAt">Created Time</TableCell>
-              <TableCell className="cell_img">Image</TableCell>
-              <TableCell className="cell_action">Action</TableCell>
-            </TableRow>
-          </TableHead>
-        </Table>
-      </TableContainer>
-      <TableContainer className="table_container_body">
-        <Table>
-          <TableBody className="table_body">
-            {usersQA?.length !== 0 &&
-              usersQA?.map((item, index: number) => {
-                const timeAgo = dayjs(item?.created_at).fromNow();
-                return (
-                  <TableRow key={item?.id}>
-                    <TableCell className="cell_no">{index + 1}</TableCell>
-                    <TableCell className="cell_id">{item?.id}</TableCell>
-                    <TableCell className="cell_content">
-                      <Tooltip title={item?.title} arrow>
-                        <span className="cell_content">
-                          {item?.subject?.length > 15 ? `${item?.subject?.slice(0, 15)}...` : item?.title}
-                        </span>
-                      </Tooltip>
-                    </TableCell>
-                    <TableCell className="cell_content">
-                      <Tooltip title={item?.content} arrow>
-                        <span className="cell_content">
-                          {item?.content?.length > 30 ? `${item?.content?.slice(0, 30)}...` : item?.content}
-                        </span>
-                      </Tooltip>
-                    </TableCell>
-                    <TableCell className="cell_createdAt">{timeAgo}</TableCell>
-                    <TableCell className="cell_img">
-                      {<img src={item?.senderInfor?.avatarUrl} alt="img" className="img" />}
-                    </TableCell>
-                    <TableCell className="cell_action">
-                      <Button onClick={() => handleAnswer(item?.id)}>Answer ❓❓❓</Button>
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        className="table_pagination"
-        component="div"
-        count={count}
-        page={page}
-        rowsPerPage={rowsPerPage}
-        rowsPerPageOptions={rowsPerPageOptions}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Box>
+    </>
   );
-};
+});
 
 export default memo(ManageUserQA);
---
+++ client/src/components/admin/manageusersQA/answerModal/index.tsx
@@ -0,0 +1,118 @@
+import { useState } from 'react';
+import { Card, CardContent, Container, Typography, TextField, Button, Grid, Box } from '@mui/material';
+import { Formik, Form, Field } from 'formik';
+import './styles.scss';
+import ReCAPTCHA from 'react-google-recaptcha';
+import { RE_CAPTCHA_SITE_KEY } from 'src/utils/helper';
+import { ToastSuccess } from 'src/utils/toastOptions';
+import { observer } from 'mobx-react';
+import { createAxios, putDataAPI } from 'src/utils';
+import accountStore from 'src/store/accountStore';
+
+const AnswerQAModal = observer(({ isOpen, questionData, setIsOpenAnswerModal }) => {
+  const [isShowBtnSend, setIsShowBtnSend] = useState(false);
+
+  const onToggleBtnSend = () => {
+    setIsShowBtnSend(true);
+  };
+
+  const account = accountStore?.account;
+
+  const setAccount = () => {
+    return accountStore?.setAccount;
+  };
+
+  const axiosJWT = createAxios(account, setAccount);
+
+  const handleSubmit = (data, { resetForm }) => {
+    console.log(data);
+    const handleAnswer = () => {
+      const qaAnswerData = {
+        qaId: questionData.id,
+        adminReplyId: account?.id,
+        content: data.content,
+      };
+      putDataAPI('/qa/reply', qaAnswerData, account.access_token, axiosJWT)
+        .then((res) => {
+          resetForm();
+          ToastSuccess('Reply Question Successfully!!!');
+          setIsOpenAnswerModal(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    handleAnswer();
+  };
+
+  const onSuccessReCaptcha = () => {
+    onToggleBtnSend();
+  };
+
+  return isOpen ? (
+    <Container className="container-answer-qa">
+      <Grid className="grid-answer-qa">
+        <Card className="card-answer-qa">
+          <CardContent>
+            <Typography variant="h4" gutterBottom>
+              Answer User Question
+            </Typography>
+            <Typography variant="subtitle1">Please answer user questions about the system!</Typography>
+          </CardContent>
+        </Card>
+        <Formik
+          initialValues={{
+            content: '',
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <Typography sx={{ color: 'black', paddingY: 1, fontSize: 24 }}>{'Question Subject:'}</Typography>
+
+                <Box sx={{ backgroundColor: 'black', padding: 2, maxHeight: 'fit-content' }}>
+                  <Typography>{questionData?.subject}</Typography>
+                </Box>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography sx={{ color: 'black', paddingY: 1, fontSize: 24 }}>{'Question Content:'}</Typography>
+
+                <Box sx={{ backgroundColor: 'black', padding: 2, maxHeight: 'fit-content' }}>
+                  <Typography>{questionData?.subject}</Typography>
+                </Box>
+              </Grid>
+              <Grid item xs={12}>
+                <Field
+                  as={TextField}
+                  required
+                  name="content"
+                  label="Reply Content"
+                  placeholder="Write your Reply Content..."
+                  variant="outlined"
+                  fullWidth
+                  multiline
+                  rows={8}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <ReCAPTCHA sitekey={RE_CAPTCHA_SITE_KEY} onChange={onSuccessReCaptcha} />,
+              </Grid>
+              {isShowBtnSend && (
+                <Grid item xs={12} className="btn-wrap">
+                  <Button className="button-Send" variant="contained" color="primary" type="submit">
+                    Reply
+                  </Button>
+                </Grid>
+              )}
+            </Grid>
+          </Form>
+        </Formik>
+      </Grid>
+    </Container>
+  ) : (
+    <></>
+  );
+});
+
+export default AnswerQAModal;
