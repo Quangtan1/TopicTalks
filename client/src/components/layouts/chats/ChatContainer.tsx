ba7eea73a8886b4c29de839ec3a3adf5c636cb00
Quangtan1 <quangtanc12345@gmail.com>
2023-11-26 23:26:08 +0700

    Lazy Loading for Message

--- client/src/components/layouts/chats/ChatContainer.tsx
+++ client/src/components/layouts/chats/ChatContainer.tsx
@@ -15,39 +15,58 @@ import SideBarMessage from './SideBarMessage';
 import Loading from 'src/components/loading/Loading';
 
 const ChatContainer = observer(() => {
-  const { setMessage } = useContext(ChatContext);
+  const { message, setMessage } = useContext(ChatContext);
   const account = accountStore?.account;
   const accountJwt = account;
   const chat = chatStore?.selectedChat;
+  const [page, setPage] = useState<number>(0);
+  const [isLast, setIsLast] = useState<boolean>(true);
+  const [isLoadMessage, setIsLoadMessage] = useState<boolean>(false);
+
   const setAccount = () => {
     return accountStore?.setAccount;
   };
   const axiosJWT = createAxios(accountJwt, setAccount);
 
+  const fetchApi = (page: number) => {
+    setIsLoadMessage(true);
+    getDataAPI(`/message/${chat.conversationInfor.id}?page=${page}&size=10`, account.access_token, axiosJWT)
+      .then((res) => {
+        const newMessage = res.data.content;
+        page === 0 ? setMessage(newMessage) : setMessage([...newMessage, ...message]);
+        setIsLoadMessage(false);
+        const lengthData = res.data.content.length;
+        (lengthData === 0 || lengthData < 10) && setIsLast(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   useEffect(() => {
     if (chat !== null && chat !== undefined) {
-      // uiStore?.setLoading(true);
-      getDataAPI(`/message/${chat.conversationInfor.id}`, account.access_token, axiosJWT)
-        .then((res) => {
-          setMessage(res.data);
-          // uiStore?.setLoading(false);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      fetchApi(0);
     }
+    return () => {
+      setPage(0);
+      setIsLast(true);
+    };
   }, [chat]);
 
+  useEffect(() => {
+    if (page !== 0 && isLast) {
+      fetchApi(page);
+    }
+  }, [page]);
+
   const isGroup = chat?.conversationInfor?.isGroupChat;
-  const isMember = isGroup ? chat?.isMember : 'true';
 
   return (
     <Box className="chat_container">
       {uiStore?.loading && <Loading />}
-
       <SideBarMessage />
       <ListConversation />
-      <ChatBox chat={chat} />
+      <ChatBox chat={chat} isLoadMessage={isLoadMessage} setPage={setPage} />
       {chat !== null && isGroup && <ConversationSetting chat={chat} />}
     </Box>
   );
