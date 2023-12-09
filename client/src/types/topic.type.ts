0d63d33958966023667c40587474ab3e14404fdc
Son Le <son_le@datahouse.com>
2023-12-09 18:03:26 +0700

    feat: integrate hot topic

--- client/src/types/topic.type.ts
+++ client/src/types/topic.type.ts
@@ -2,8 +2,22 @@ export interface ListTopic {
   id: number;
   topicParentName: string;
   image: string;
+  avgRating?: number;
   shortDescript: string;
   createdAt: string;
+}
+
+export interface ListTopicHot {
+  id: number;
+  avgRating: number | null;
+  createdAt: string;
+  image: string;
+  maxRating: number;
+  shortDescription: string;
+  topicChildrenId: number;
+  topicChildrenName: string;
+  tpcCount: number;
+  updatedAt: string;
 }
 
 export interface TopicChild {
